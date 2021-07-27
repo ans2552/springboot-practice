9fe37f9168a89023b21b14a03d68d5db32788e4b
ans2552 <hja5432@gmail.com>
2021-07-27 14:06:43 +0900

    delete jUnit4

--- src/test/java/com/ans2552/practice/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ans2552/practice/springboot/web/HelloControllerTest.java
@@ -18,7 +18,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws  Exception {
+    public void returnHello() throws  Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
