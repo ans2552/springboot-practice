9adb821a1de3f679ea3a71b6a2d2e1f818708a72
ans2552 <hja5432@gmail.com>
2021-07-27 14:01:26 +0900

    add HelloController and jUnit test

---
+++ src/main/java/com/ans2552/practice/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.ans2552.practice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/ans2552/practice/springboot/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.ans2552.practice.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
