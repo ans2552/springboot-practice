3d67518b301dabd0b091b624adfedbfc7b9bbf08
ans2552 <hja5432@gmail.com>
2021-08-03 18:03:34 +0900

    add Posts Entity Test

---
+++ src/test/java/com/ans2552/practice/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.ans2552.practice.springboot.web.domain.posts;
+
+import com.ans2552.practice.springboot.domain.posts.Posts;
+import com.ans2552.practice.springboot.domain.posts.PostsRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void post_load() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("hja5432@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+
+}
