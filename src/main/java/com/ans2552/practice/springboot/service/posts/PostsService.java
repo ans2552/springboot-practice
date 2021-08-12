dc1217773aae580f800a427731edb992b4b40e63
ans2552 <hja5432@gmail.com>
2021-08-13 01:44:02 +0900

    add Posts get api

--- src/main/java/com/ans2552/practice/springboot/service/posts/PostsService.java
+++ src/main/java/com/ans2552/practice/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.ans2552.practice.springboot.service.posts;
 
 import com.ans2552.practice.springboot.domain.posts.Posts;
 import com.ans2552.practice.springboot.domain.posts.PostsRepository;
+import com.ans2552.practice.springboot.web.dto.PostsResponseDto;
 import com.ans2552.practice.springboot.web.dto.PostsSaveRequestDto;
 import com.ans2552.practice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -20,9 +21,15 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
 
-        posts.update(requestDto.getTitle(),requestDto.getContent());
+        posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/ans2552/practice/springboot/web/PostsApiController.java
+++ src/main/java/com/ans2552/practice/springboot/web/PostsApiController.java
@@ -1,6 +1,7 @@
 package com.ans2552.practice.springboot.web;
 
 import com.ans2552.practice.springboot.service.posts.PostsService;
+import com.ans2552.practice.springboot.web.dto.PostsResponseDto;
 import com.ans2552.practice.springboot.web.dto.PostsSaveRequestDto;
 import com.ans2552.practice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +19,11 @@ public class PostsApiController {
 
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable long id, @RequestBody PostsUpdateRequestDto requestDto) {
-        return postsService.update(id,requestDto);
+        return postsService.update(id, requestDto);
     }
 
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable long id) {
+        return postsService.findById(id);
+    }
 }
