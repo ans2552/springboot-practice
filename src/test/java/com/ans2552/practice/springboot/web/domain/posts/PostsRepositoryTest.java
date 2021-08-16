6304ef1b146bc4a478b727eec5b5d29a01966fab
ans2552 <hja5432@gmail.com>
2021-08-17 00:05:57 +0900

    add Jpa auditing test

--- src/test/java/com/ans2552/practice/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/ans2552/practice/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,7 +22,7 @@ public class PostsRepositoryTest {
     PostsRepository postsRepository;
 
     @AfterEach
-    public void cleanup(){
+    public void cleanup() {
         postsRepository.deleteAll();
     }
 
@@ -43,5 +44,20 @@ public class PostsRepositoryTest {
 
     }
 
+    @Test
+    public void post_BaseTimeEntitiy() {
+        LocalDateTime now = LocalDateTime.of(2021,8,17,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
 
+        List<Posts> postsList = postsRepository.findAll();
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>> createDate=" + posts.getCreatedDate() +", modifiedDate="+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
