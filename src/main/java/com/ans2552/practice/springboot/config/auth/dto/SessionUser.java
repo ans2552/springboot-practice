0bf26abd4272cb56be494bd72c769e627e977d93
ans2552 <hja5432@gmail.com>
2021-09-01 15:38:35 +0900

    add oauth google login

---
+++ src/main/java/com/ans2552/practice/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.ans2552.practice.springboot.config.auth.dto;
+
+import com.ans2552.practice.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
